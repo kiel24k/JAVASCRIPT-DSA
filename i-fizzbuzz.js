852c95552a39cae73358cdbc7135e01a47def260
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-09-24 13:09:46 +0800

    fizzbuzz added

---
+++ i-fizzbuzz.js
@@ -0,0 +1,18 @@
+//print numbers from 1 to n
+//if number is divisible by 3, print "Fizz"
+//if number is divisible by 5, print "Buzz"
+//if number is divisible by 3 and 5, print "FizzBuzz"
+//else, print the number
+// console.log(8 % 5);
+
+const fizzBuzz = (n) => {
+    for(let i = 1; i < n + 1; i++){
+        if( i % 5){
+            console.log("fuzzbizz")
+        }else{
+            console.log(i);
+        }
+    }
+}
+console.log(fizzBuzz(15));
+
