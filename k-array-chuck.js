33f563971766b66eb8f8540f15f3b0b1da1c2e50
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-09-26 22:53:58 +0800

    array chunk added => done

--- k-array-chuck.js
+++ k-array-chuck.js
@@ -4,3 +4,32 @@
 //chunk ([1, 2, 3 ,4 ,5 ,6 ,7, 8], 3)  [[1,2,3] , [4,5,6,], [7,8]]
 
 //chunkArray[(1,2,3,4,5),2] //Output: [[1,2,], [3,4]]
+
+// 1. Create an empty array to hold the chunks
+// 2. Set a string index to keep track of where we are in the original array
+// 3. Loop through the original array as long as the index hasn't reacched the end
+// 4. Extract a chunk of the desired size from the original array
+// 5. Add the extracted chunk to the 'chucked' array
+// 6. Move the index forward by the chunk size to get to the next chunk
+// 7. Return the final array of chunks
+
+const chunkArray = []
+
+const chunk = (array, size) => {
+   const chunked = []
+   let index = 0;
+
+   while(index < array.length){
+    const chunk =  array.slice(index, index + size)
+    console.log('-----',chunk);
+    chunked.push(chunk)
+    index += size
+   }
+
+   return chunked
+    
+}
+
+console.log(chunk([1,2,3,4,5,6,7,1], 1));
+
+
