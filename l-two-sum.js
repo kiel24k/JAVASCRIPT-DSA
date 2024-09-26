c3220acec3d0c269bd15a3aef68acbdd8a2a7c4c
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-09-26 23:09:22 +0800

    two sum file added

---
+++ l-two-sum.js
@@ -0,0 +1,5 @@
+//two sum
+// Imagine you have a list of numbers and a target nunber. Your job is to find two numbers in that list that add up to the target number. You also need to tell which positions (or indexes) those two numbers are at in the list.
+
+//Example 
+// if the list is [2,7,11,15] and the target is 9, then the answer would be, [0,1] because 2 (at index 0) plus 7 (at index 1) equals 9.
