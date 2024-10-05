4ac8c4ac51913be4d8dc49c3e91e74d7fe603146
Kiel Alarcon Bermudez <kielbermudez200104@gmail.com>
2024-10-05 14:48:21 +0800

    join arrays added

---
+++ JS-Arrays/join.js
@@ -0,0 +1,34 @@
+const normalArray = [1,5,3,4,5]
+
+const list = [
+    {
+        name: 'Juan',
+        age: 22,
+        favorite_num: 1
+    },
+    {
+        name: 'Michael',
+        age: 23,
+        favorite_num: 2,
+    },
+    {
+        name: 'France',
+        age: 19,
+        favorite_num: 3
+    },
+    {
+        name: 'Amor',
+        age: 18,
+        favorite_num: 4
+    },
+    {
+        name: 'kiel',
+        age: 22,
+        favorite_num: 5
+    }
+]
+
+const join = normalArray.join(', ')
+console.log(join);
+
+
