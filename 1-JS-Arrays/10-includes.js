b5e1eb6c01775b95fdefd3119cd5a725a9ad957d
Kelly <149242693+kiel24k@users.noreply.github.com>
2024-11-05 23:24:47 +0800

    Update 10-includes.js

--- 1-JS-Arrays/10-includes.js
+++ 1-JS-Arrays/10-includes.js
@@ -30,7 +30,7 @@ const list = [
 const normalArray = [1,2,3,4,5,'six',7,'eight']
 
 
-const includes = normalArray.includes('six')
+const includes = normalArray.includes('six');
 console.log(includes);
 
 
