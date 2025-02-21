d2d294d46e826c948a67ea890df4395d71111746
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-02-21 23:11:41 +0800

    Update a-problem.js

--- a-problem.js
+++ a-problem.js
@@ -2,7 +2,7 @@
 
 //create an array with 5 students names,after that create a function which takes 2 parameters (allStudents & studentName) iterate over all students and find that specific user "studentName"
 
-const studentDatabase = ["jordan", "erick", "john", "michel"];
+const studentDatabase = ["jordan", "erick", "john", "michael"];
 
 const findStudent = (allStudents, studentName) => {
   for (let i = 0; i < allStudents.length; i++) {
