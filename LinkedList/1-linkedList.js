1c2091ae67c465a5c9c1b71f236d7dc52ccd852e
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-02-25 23:48:54 +0800

    Update 1-linkedList.js

--- LinkedList/1-linkedList.js
+++ LinkedList/1-linkedList.js
@@ -4,7 +4,7 @@ hclass Node {
         this.next = null
     }
 }
-//kiel the greathnhudsadsasdaddhhhjhjdasdsaaadadssdsaad
+//kiel the greathnhudsadsasdaddhhhjhjdasdsaaadadssdsaaddasd
 
 class LinkedList {
     constructor(value){
