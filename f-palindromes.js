4eb021a657f2e9c2424aeed0ce99f0ad8145fb49
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-02-25 23:40:43 +0800

    Update f-palindromes.js

--- f-palindromes.js
+++ f-palindromes.js
@@ -4,5 +4,5 @@
 const palindrome = (str) => {
    return str.split("").reverse().join("") === str
 }
-console.log(palindrome("saldldsdddsadsadadasads"));
+console.log(palindrome("saldldsdddsadsadads"));
 console.log(palindrome("cddcdsadsdsda"));
