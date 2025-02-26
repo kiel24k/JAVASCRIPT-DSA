1628446fe78388a70ece8b9dff7fd623af28c762
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-02-26 22:01:00 +0800

    Update b-O(n).js

--- b-O(n).js
+++ b-O(n).js
@@ -1,6 +1,6 @@
 const groceries = ["milk", "bread", "eggs", "flour", "choose", "sugar"];
 
-//O(n) - stop execution when item is found dasdasdsadad f
+//O(n) - stop execution when item is found dasdasdsadad fdsd
 
 const searchForItem = (item) => {
   for (let i = 0; i < groceries.length; i++) {
