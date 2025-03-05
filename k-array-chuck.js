c421fd92b00ac6edf9298bc1e5652c800580b354
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-03-05 23:15:57 +0800

    Update k-array-chuck.js

--- k-array-chuck.js
+++ k-array-chuck.js
@@ -1,4 +1,4 @@
-//array chuck
+no//array chuck
 //write a function that takes an array and chunk size as input the function should return a new array where the     original array is split into chuinks of the specified size.
 
 //chunk ([1, 2, 3 ,4 ,5 ,6 ,7, 8], 3)  [[1,2,3] , [4,5,6,], [7,8]]
@@ -29,7 +29,7 @@ const chunk = (array, size) => {
    return chunked
     
 }
-//sige kiel hahahayh
+//sige kiel hahah
 
 console.log(chunk([1,2,3,4,5,6,7,1], 1));
 
