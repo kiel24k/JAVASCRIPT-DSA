bae20467e4dc40b67ec8217a5ba5fd790a71f0b3
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-04-18 21:52:48 +0800

    Update reduceRight.js

--- ArraysTesting/reduceRight.js
+++ ArraysTesting/reduceRight.js
@@ -2,4 +2,4 @@ const arraySample = [1,2,4,5,6,7,8,'nine','ten',11]
 
 const reduceRight = arraySample.reduceRight((total, el) => total + el, 0 )
 console.log(reduceRight);
-//jsjssssssssaoszasxxwijxxjs
+//jsjssssssssaoszasxxwijxxjsszh
