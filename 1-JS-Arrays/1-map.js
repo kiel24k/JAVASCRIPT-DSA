00c3f4df28f524eea96141ca747f422f519c3bda
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-06-17 22:57:14 +0800

    Update 1-map.js

--- 1-JS-Arrays/1-map.js
+++ 1-JS-Arrays/1-map.js
@@ -27,5 +27,5 @@ const map = list.map((e) => {
    return  e.favorite_num + 2
 })
 console.log(map)
-//kiel  dasdas dasdas dsadd das
+//kiel  dasdas dasdas dsadd das dasdas
 //Kiel dasda dsadas dasdas dasdas
