bd92fd5351c3c21a74a2b2677a03ddebb0bb3a59
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-06-20 23:29:33 +0800

    Update 1-map.js

--- 1-JS-Arrays/1-map.js
+++ 1-JS-Arrays/1-map.js
@@ -28,4 +28,4 @@ const map = list.map((e) => {
 })
 console.log(map)
 //kielkiel   dasdasdasdas dasdas dasd dasd dsada
-//Kielddssxcdssss ss dsadas dsadas dasda dsad dasd
+//Kielddssxcdssss ss dsadas dsadas dasda dsad dasd dasd
