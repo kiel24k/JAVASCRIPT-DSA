714e29490505950d2ae3d0001b227f456dfc9d25
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-06-24 23:27:08 +0800

    Update 11-pop.js

--- 1-JS-Arrays/11-pop.js
+++ 1-JS-Arrays/11-pop.js
@@ -31,7 +31,7 @@ const normalArray = [1,2,3,4,5]
 //Removes the last element from an array and returns that element.
 const pop = normalArray.pop()
 console.log(normalArray);
-//sssssdessxxxddcdvc cdf
+//sssssdessxxxddcdvc cdfdd
 
 
 
