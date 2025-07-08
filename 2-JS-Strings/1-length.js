fd0a03db71d74815368b16ea5a4eb1e4cdf632c7
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-07-08 22:35:17 +0800

    Update 1-length.js

--- 2-JS-Strings/1-length.js
+++ 2-JS-Strings/1-length.js
@@ -2,5 +2,5 @@ slet text = "Kiel Alarcon Bermudeazzszds"
 //The **length **property returns the length of a string:
 let length = text.length
 console.log(length)
-//jajdscssws2322323232232
+//jajdscssws2322323232232232
 //asdsdwssss2232231231213121232
