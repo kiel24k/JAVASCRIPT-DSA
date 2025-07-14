92b76749a2a053e2f1799a4d3c06919d1cfc318c
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-07-14 22:50:07 +0800

    Update 1-map.js

--- 1-JS-Arrays/1-map.js
+++ 1-JS-Arrays/1-map.js
@@ -26,6 +26,6 @@ const map = list.map((e) => {
    return  e.favorite_num + 2
 })
 console.log(map) dasdas
-//kiel bermudez alarcon fsfsadasdas
+//kiel bermudez alarcon fsfsadasdasdsadas
 //kieldfasdfdfsadffsfasdsafasdasdasda
 
