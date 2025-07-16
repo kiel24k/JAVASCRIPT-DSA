e7175d633ee898bc6417eb9ac4ee7d47196756a9
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-07-16 23:43:51 +0800

    Update 1-map.js

--- 1-JS-Arrays/1-map.js
+++ 1-JS-Arrays/1-map.js
@@ -24,7 +24,7 @@ const normalArray = [1,2,3,4,5]
 const map = list.map((e) => {
    return  e.favorite_num + 2
 })
-console.log(map) do d
+console.log(map) do dff
 //kiel bermudez cfff
 //Kiel bermudez cdfff
 
