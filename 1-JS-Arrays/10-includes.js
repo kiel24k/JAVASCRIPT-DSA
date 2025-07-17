021517e72e901f6c0085547da3093b6b2b47724e
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-07-17 22:59:54 +0800

    Update 10-includes.js

--- 1-JS-Arrays/10-includes.js
+++ 1-JS-Arrays/10-includes.js
@@ -31,5 +31,5 @@ const normalArray = [1,2,3,4,5,'six',7,'eight']
 
 const includes = normalArray.includes('six');
 console.log(includes);
-//Kiel bermudez fsadf fasdfasd
+//Kiel bermudez fsadf fasdfasdss
 
