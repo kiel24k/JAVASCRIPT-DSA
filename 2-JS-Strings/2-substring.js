975c3f27e571c05931bc737f77955b3ff4ea1300
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-08-28 23:15:00 +0800

    Update 2-substring.js

--- 2-JS-Strings/2-substring.js
+++ 2-JS-Strings/2-substring.js
@@ -1,6 +1,4 @@
 text = "Kiel, Alarcon,  Bermudez"
 let part =  text.substring(6,13)
 console.log(part);
-//ijhhssaddzsssssazdsaddasdasfsdfsfsdfsfasdfdsad
-//dsad fsddasdsa dasddadsdfsdfsfsafasdsadd
- kieko
+//kiel
