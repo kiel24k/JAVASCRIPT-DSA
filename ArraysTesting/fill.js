ddd24f204639d52d9d4b8ae0d6cdac216b2fb625
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-09-08 23:34:13 +0800

    Update fill.js

--- ArraysTesting/fill.js
+++ ArraysTesting/fill.js
@@ -1,4 +1,4 @@
-jjfconst arraySample = [1,2,4,5,1,1,82,'niddne','ten',11]
+ggjjfconst arraySample = [1,2,4,5,1,1,82,'niddne','ten',11]
 //hhhxiwxissxxsxsxs
 const fill = arraySample.fill(0)
 console.log(
