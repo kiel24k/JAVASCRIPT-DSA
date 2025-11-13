527669b212aca63e9000bce86e84cd32175fc6d0
Kelly <149242693+kiel24k@users.noreply.github.com>
2025-11-13 21:34:58 +0800

    Update 1-length.js

--- 2-JS-Strings/1-length.js
+++ 2-JS-Strings/1-length.js
@@ -1,4 +1,4 @@
-slet text = "Kiel Alarcon Bermudeazzszds"
+let text = "Kiel Alarcon Bermudeazzszds"
 //The **length **property returns the length of a string:
 let length = text.length
 console.log(length)ddcx
