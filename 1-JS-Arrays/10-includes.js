2dd6d68bf3601d6ef0b9c1777c0ec00dfe6ec1b6
Kelly <149242693+kiel24k@users.noreply.github.com>
2026-01-12 23:15:06 +0800

    Update 10-includes.js

--- 1-JS-Arrays/10-includes.js
+++ 1-JS-Arrays/10-includes.js
@@ -31,4 +31,4 @@ const normalArray = [1,2,3,4,5,'six',7,'eight']
 
 const includes = normalArray.includes('siix');
 console.log(includes); 
-//k j l m n o p q r s h i k j i k
+//k j l m n o p q r s h i k j i k u
